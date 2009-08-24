3de319fd745ea7e7e1c196902d1f52520a4af098
felipe.lessa <felipe.lessa@gmail.com>
2009-08-24 16:21:12 +0000

    Performance improvements (?!)

--- Math/Statistics/Dirichlet.hs
+++ Math/Statistics/Dirichlet.hs
@@ -83,44 +83,45 @@ deriveDD :: Predicate -> StepSize
          -> [TrainingVector] -> ST s (Result DirichletDensity)
 deriveDD _ _ [] = error "Dirichlet.deriveDD: empty training data"
 deriveDD (Pred maxIter' minDelta') (Step step) trainingData
- = join $ return train `ap` filled size (exp 1) `ap` filled size 1
+ = join $ return train `ap` arr 1 `ap` arr (exp 1) `ap` arr (exp 1)
     where
+      arr  = filled size
       size = lengthU (head trainingData)
-      trainingSize   = fromIntegral $ length trainingData
+      !trainingSize  = fromIntegral $ length trainingData
       trainingCounts = map (\t -> t :*: sumU t) trainingData
 
-      train !as !ws = go 0
-          where
-            go !iter = do
-              -- Precalculate values that don't change here
-              sumAs <- sumMU size as
-              let psiSumAs = psi sumAs
-
-              -- Reestimate w's
-              forM_ [0..size-1] $ \i -> do
-                  w_old <- readMU ws i
-                  a_i   <- readMU as i
-                  let s1 = trainingSize * (psiSumAs - psi a_i)
-                      s2 = sum [psi (indexU t i + a_i) - psi (sumT + sumAs)
-                                    | t :*: sumT <- trainingCounts]
-                      w_new = w_old + step * a_i * (s1 + s2)
-                  writeMU ws i w_new
-
-              -- Calculate new alpha's
-              delta <- calculateAlphas 0 0
+      train = let sumAs = fromIntegral size * exp 1
+              in train' 0 sumAs (psi sumAs)
+      train' !iter !sumAs !psiSumAs !ws !as !new_as = do
+              -- Reestimate alpha's
+              (newDelta, newSumAs) <- calculateAlphas 0 0 0
 
               -- Verify convergence
-              case (delta < minDelta', iter >= maxIter') of
-                (True, _) -> Result Delta   iter delta <$> finish
-                (_, True) -> Result MaxIter iter delta <$> finish
-                _         -> go (iter+1)
-              where finish = DD <$> unsafeFreezeAllMU as
+              case (newDelta < minDelta', iter >= maxIter') of
+                (True, _) -> Result Delta   iter newDelta <$> finish
+                (_, True) -> Result MaxIter iter newDelta <$> finish
+                _         -> train' (iter+1) newSumAs (psi newSumAs) ws new_as as
+                             -- yes! we swap as with new_as
+          where
+            finish = DD <$> unsafeFreezeAllMU as
 
-            calculateAlphas !i !acc
-                | i == size = return acc
-                | otherwise = do old_a <- readMU as i
-                                 new_w <- readMU ws i
-                                 let new_a = exp new_w
-                                 writeMU as i new_a
-                                 let delta = abs (old_a - new_a)
-                                 calculateAlphas (i+1) (max acc delta)
+            calculateAlphas !i !newDelta !newSumAs
+                | i == size = return (newDelta, newSumAs)
+                | otherwise = do
+              -- Old values
+              w_old <- readMU ws i
+              a_old <- readMU as i
+
+              -- New values
+              let s1 = trainingSize * (psiSumAs - psi a_old)
+                  s2 = sum [psi (indexU t i + a_old) - psi (sumT + sumAs)
+                                | t :*: sumT <- trainingCounts]
+                  w_new = w_old + step * a_old * (s1 + s2)
+                  a_new = exp w_new
+
+              -- Write, calculate delta & sum
+              writeMU ws     i w_new
+              writeMU new_as i a_new
+              let delta = abs (a_old - a_new)
+              calculateAlphas (i+1) (max newDelta delta) (newSumAs + a_new)
+
