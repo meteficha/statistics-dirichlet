8a61031c5091161b56a8e175ae8421a6029485f8
felipe.lessa <felipe.lessa@gmail.com>
2009-08-27 01:27:47 +0000

    Muitas mudanças sem commit separado.

--- Math/Statistics/Dirichlet.hs
+++ Math/Statistics/Dirichlet.hs
@@ -47,81 +47,120 @@ data Predicate = Pred
     {maxIter  :: !Int    -- ^ Maximum number of iterations.
     ,minDelta :: !Double -- ^ Minimum delta to continue iterating.
     }
+                 deriving (Eq, Show)
 
 -- | Reason why the derivation was over.
 data Reason = Delta | MaxIter
+              deriving (Eq, Show, Enum)
 
 -- | Result of a deriviation.
 data Result a = Result {reason    :: !Reason
                        ,iters     :: !Int
                        ,lastDelta :: !Double
+                       ,lastCost  :: !Double
                        ,result    :: !a}
+                deriving (Eq, Show)
+
+
+
+
+
+-- | Sum of a mutable array.  Uses Kahan summation algorithm.
+kahanSumMU :: MUArr Double s -> ST s Double
+kahanSumMU !arr | size >= 1 = readMU arr 0 >>= go 1 0
+                | otherwise = return 0
+    where go !i !comp !acc
+              | i == size = return acc
+              | otherwise = do x <- readMU arr i
+                               let x' = x - comp
+                                   acc'  = acc + x'
+                                   comp' = (acc' - acc) - x'
+                               go (i+1) comp' acc'
+          size = lengthMU arr
+
+-- | Sum of a list.  Uses Kahan summation algorithm.
+kahanSum :: [Double] -> Double
+kahanSum []     = 0
+kahanSum (y:ys) = go 0 y ys
+    where go !comp !acc (x:xs) = let x' = x - comp
+                                     acc'  = acc + x'
+                                     comp' = (acc' - acc) - x'
+                                 in go comp' acc' xs
+          go _ acc [] = acc
+
 
 
 
 
 -- | A Dirichlet density.
-newtype DirichletDensity = DD (UArr Double)
-
--- | Returns a new filled 'MUArr'.
-filled :: Int -> Double -> ST s (MUArr Double s)
-filled size !value = newMU size >>= fill
-    where fill !arr = go 0
-              where go n | n == size = return arr
-                         | otherwise = writeMU arr n value >> go (n+1)
-
--- | Sum of a mutable array.
-sumMU :: Int -> MUArr Double s -> ST s Double
-sumMU size !arr = go 0 0
-    where go !x !acc | x == size = return acc
-                     | otherwise = readMU arr x >>= go (x+1) . (acc+)
+data DirichletDensity = DD !(UArr Double)
+                        deriving (Eq, Show)
 
 -- | Derive a Dirichlet density using a maximum likelihood method
 --   as described by Karplus et al.  All training vectors should
 --   have the same length, however this is not verified.
-deriveDD :: Predicate -> StepSize
-         -> [TrainingVector] -> ST s (Result DirichletDensity)
-deriveDD _ _ [] = error "Dirichlet.deriveDD: empty training data"
-deriveDD (Pred maxIter' minDelta') (Step step) trainingData
- = join $ return train `ap` arr 1 `ap` arr (exp 1) `ap` arr (exp 1)
+deriveDD :: DirichletDensity -> Predicate -> StepSize
+         -> [TrainingVector] -> Result DirichletDensity
+deriveDD _ _ _ [] = error "Dirichlet.deriveDD: empty training data"
+deriveDD (DD initial) (Pred maxIter' minDelta') (Step step) trainingData = runST train
     where
-      arr  = filled size
       size = lengthU (head trainingData)
       !trainingSize  = fromIntegral $ length trainingData
       trainingCounts = map (\t -> t :*: sumU t) trainingData
 
-      train = let sumAs = fromIntegral size * exp 1
-              in train' 0 sumAs (psi sumAs)
-      train' !iter !sumAs !psiSumAs !ws !as !new_as = do
-              -- Reestimate alpha's
-              (newDelta, newSumAs) <- calculateAlphas 0 0 0
+      train = do
+        -- Initialization
+        [ws, as, new_as] <- replicateM 3 (newMU size)
+        copyMU as 0 initial
+        copyMU ws 0 (mapU log initial)
+        sumAs <- kahanSumMU as
+        train' 0 1e100 sumAs (psi sumAs) ws as new_as
+      train' !iter !oldCost !sumAs !psiSumAs !ws !as !new_as = do
+        -- Reestimate alpha's
+        mapM_ calculateAlphas [0..size-1]
+        newSumAs <- kahanSumMU new_as
+        newCost  <- costDD' new_as newSumAs trainingCounts
 
-              -- Verify convergence
-              case (newDelta < minDelta', iter >= maxIter') of
-                (True, _) -> Result Delta   iter newDelta <$> finish
-                (_, True) -> Result MaxIter iter newDelta <$> finish
-                _         -> train' (iter+1) newSumAs (psi newSumAs) ws new_as as
-                             -- yes! we swap as with new_as
-          where
-            finish = DD <$> unsafeFreezeAllMU as
+        -- Verify convergence
+        let delta = abs (newCost - oldCost)
+        case (delta <= minDelta', iter >= maxIter') of
+          (True, _) -> Result Delta   iter delta newCost <$> finish
+          (_, True) -> Result MaxIter iter delta newCost <$> finish
+          _         -> train' (iter+1) newCost newSumAs (psi newSumAs) ws new_as as
+                       -- yes! we swap as with new_as
+       where
+         finish = DD <$> unsafeFreezeAllMU new_as
+         calculateAlphas !i = do
+           -- Old values
+           w_old <- readMU ws i
+           a_old <- readMU as i
 
-            calculateAlphas !i !newDelta !newSumAs
-                | i == size = return (newDelta, newSumAs)
-                | otherwise = do
-              -- Old values
-              w_old <- readMU ws i
-              a_old <- readMU as i
+           -- New values
+           let s1 = trainingSize * (psiSumAs - psi a_old)
+               s2 = do t :*: sumT <- trainingCounts
+                       [psi (t `indexU` i + a_old), -psi (sumT + sumAs)]
+               w_new = w_old + step * a_old * kahanSum (s1 : s2)
+           writeMU ws     i w_new
+           writeMU new_as i (exp w_new) -- do not write in 'as'!
 
-              -- New values
-              let s1 = trainingSize * (psiSumAs - psi a_old)
-                  s2 = sum [psi (indexU t i + a_old) - psi (sumT + sumAs)
-                                | t :*: sumT <- trainingCounts]
-                  w_new = w_old + step * a_old * (s1 + s2)
-                  a_new = exp w_new
+-- | Cost function for deriving a Dirichlet density.  This function is minimized.
+costDD :: DirichletDensity -> [TrainingVector] -> Double
+costDD (DD arr) tv = runST (go $ map (\t -> t :*: sumU t) tv)
+    where
+      size = lengthU arr
+      go tc = do alphas <- newMU size
+                 copyMU alphas 0 arr
+                 sumAs <- kahanSumMU alphas
+                 costDD' alphas sumAs tc
 
-              -- Write, calculate delta & sum
-              writeMU ws     i w_new
-              writeMU new_as i a_new
-              let delta = abs (a_old - a_new)
-              calculateAlphas (i+1) (max newDelta delta) (newSumAs + a_new)
-
+costDD' :: MUArr Double s -> Double -> [TrainingVector :*: Double] -> ST s Double
+costDD' alphas sumAs trainingCounts =
+    let lngammaSumAs = lngamma sumAs
+        size = lengthMU alphas
+        f (t :*: sumT) = go 0 $ lngamma (sumT+1) + lngammaSumAs - lngamma (sumT + sumAs)
+            where go !i !acc | i == size = return acc
+                             | otherwise = do
+                    a_i <- readMU alphas i
+                    let t_i = t `indexU` i
+                    go (i+1) $ acc + lngamma (t_i + a_i) - lngamma (t_i + 1) - lngamma a_i
+    in (negate . kahanSum) `fmap` mapM f trainingCounts
