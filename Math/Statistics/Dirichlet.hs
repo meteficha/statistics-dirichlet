1d483253f339b72035ed546815e327466b2ea69b
felipe.lessa <felipe.lessa@gmail.com>
2010-02-15 21:42:09 +0000

    Use vector insted of uvector.

--- Math/Statistics/Dirichlet.hs
+++ Math/Statistics/Dirichlet.hs
@@ -25,35 +25,30 @@ module Math.Statistics.Dirichlet
     ,logBeta)
     where
 
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as MG
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as MU
+
 import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
 import Control.Parallel.Strategies (NFData(..), rwhnf)
-import Data.Array.Vector
 import Numeric.GSL.Special.Gamma (lngamma, lnbeta)
 import Numeric.GSL.Special.Psi (psi)
 
 
 
--- | Internal data type used to pair two doubles.  We don't use
---   the polymorphic (:*:) because GHC 6.10 can't unbox function
---   returns (see Don's statistics-fusion).
-data DoublePair = (:**:) {-# UNPACK #-} !Double {-# UNPACK #-} !Double
-
-instance NFData DoublePair where
-    rnf = rwhnf
-
 -- | Logarithm of the beta function applied to a vector.
-logBeta :: UArr Double -> Double
-logBeta xs | lengthU xs == 2 = lnbeta (headU xs) (lastU xs)
-           | otherwise       = fin $ foldlU go (0 :**: 0) xs
-    where
-      go  (val :**: sumXs) x = (val + lngamma x) :**: (sumXs + x)
-      fin (val :**: sumXS)   = val - lngamma sumXS
+logBeta :: U.Vector Double -> Double
+logBeta xs | U.length xs == 2 = lnbeta (U.head xs) (U.last xs)
+           | otherwise        = U.sum (U.map lngamma xs) - lngamma (U.sum xs)
+{-# INLINE logBeta #-}
 
 -- | A vector used for deriving the parameters of a Dirichlet
 --   density or mixture.
-type TrainingVector = UArr Double
+type TrainingVector = U.Vector Double
 
 -- | Usually denoted by lowercase greek letter eta (η), size of
 --   each step in the gradient. Should be greater than zero and
@@ -87,39 +82,29 @@ instance NFData a => NFData (Result a) where
     rnf = rnf . result
 
 
+--------------
+sumMU :: Int -> MU.MVector s Double -> ST s Double
+sumMU size vector = go size 0
+    where
+      go i s | s `seq` i < 0 = return s
+             | otherwise     = do x <- MU.read vector i
+                                  go i (s+x)
 
+mutable :: U.Vector Double -> ST s (MU.MVector s Double)
+mutable = MG.unstream . G.stream
 
-
--- | Sum of a mutable array.  Uses Kahan summation algorithm.
-kahanSumMU :: MUArr Double s -> ST s Double
-kahanSumMU !arr | size >= 1 = readMU arr 0 >>= go 1 0
-                | otherwise = return 0
-    where go !i !comp !acc
-              | i == size = return acc
-              | otherwise = do x <- readMU arr i
-                               let x' = x - comp
-                                   acc'  = acc + x'
-                                   comp' = (acc' - acc) - x'
-                               go (i+1) comp' acc'
-          size = lengthMU arr
-
--- | Sum of a list.  Uses Kahan summation algorithm.
-kahanSum :: [Double] -> Double
-kahanSum []     = 0
-kahanSum (y:ys) = go 0 y ys
-    where go !comp !acc (x:xs) = let x' = x - comp
-                                     acc'  = acc + x'
-                                     comp' = (acc' - acc) - x'
-                                 in go comp' acc' xs
-          go _ acc [] = acc
-
-
+immutable :: MU.MVector s Double -> ST s (U.Vector Double)
+immutable v = do let n = MU.length v
+                 r <- MU.new n
+                 MU.copy r v
+                 G.unsafeFreeze r
+--------------
 
 
 
 -- | A Dirichlet density.
-data DirichletDensity = DD !(UArr Double)
-                        deriving (Eq, Read, Show)
+data DirichletDensity = DD !(U.Vector Double)
+                        deriving (Eq, Show)
 
 instance NFData DirichletDensity where
     rnf = rwhnf
@@ -127,32 +112,36 @@ instance NFData DirichletDensity where
 -- | @emptyDD n x@ is an \"empty\" Dirichlet density with size
 --   @n@ and all alphas set to @x@.
 emptyDD :: Int -> Double -> DirichletDensity
-emptyDD = (DD .) . replicateU
+emptyDD = (DD .) . U.replicate
 
 -- | Derive a Dirichlet density using a maximum likelihood method
 --   as described by Karplus et al.  All training vectors should
 --   have the same length, however this is not verified.
 deriveDD :: DirichletDensity -> Predicate -> StepSize
-         -> [TrainingVector] -> Result DirichletDensity
-deriveDD _ _ _ [] = error "Dirichlet.deriveDD: empty training data"
+         -> V.Vector TrainingVector -> Result DirichletDensity
+deriveDD _ _ _ t | V.length t == 0 = error "Dirichlet.deriveDD: empty training data"
 deriveDD (DD initial) (Pred maxIter' minDelta') (Step step) trainingData = runST train
     where
-      size = lengthU initial
-      !trainingSize  = fromIntegral $ length trainingData
-      trainingCounts = map (\t -> t :*: sumU t) trainingData
+      size = U.length initial
+      !trainingSize  = fromIntegral $ V.length trainingData
+      trainingCounts = V.map (\t -> (t, U.sum t)) trainingData
 
       train = do
         -- Initialization
-        [ws, as, new_as] <- replicateM 3 (newMU size)
-        copyMU as 0 initial
-        copyMU ws 0 (mapU log initial)
-        sumAs <- kahanSumMU as
+        as <- mutable initial
+        ws <- mutable $ U.map log initial
+        new_as <- MU.new size
+        sumAs <- sumMU size as
         train' 0 1e100 sumAs (psi sumAs) ws as new_as
+      train' :: Int -> Double -> Double -> Double -> MU.MVector s Double
+             -> MU.MVector s Double -> MU.MVector s Double
+             -> ST s (Result DirichletDensity)
       train' !iter !oldCost !sumAs !psiSumAs !ws !as !new_as = do
         -- Reestimate alpha's
         mapM_ calculateAlphas [0..size-1]
-        newSumAs <- kahanSumMU new_as
-        newCost  <- costDD' new_as newSumAs trainingCounts
+        newSumAs <- sumMU size new_as
+        new_as'  <- immutable new_as
+        let newCost = costDD' size new_as' newSumAs trainingCounts
 
         -- Verify convergence
         let delta = abs (newCost - oldCost)
@@ -162,43 +151,36 @@ deriveDD (DD initial) (Pred maxIter' minDelta') (Step step) trainingData = runST
           _         -> train' (iter+1) newCost newSumAs (psi newSumAs) ws new_as as
                        -- yes! we swap as with new_as
        where
-         finish = DD <$> unsafeFreezeAllMU new_as
+         finish = DD <$> G.unsafeFreeze new_as
          calculateAlphas !i = do
            -- Old values
-           w_old <- readMU ws i
-           a_old <- readMU as i
+           w_old <- MU.read ws i
+           a_old <- MU.read as i
 
            -- New values
            let s1 = trainingSize * (psiSumAs - psi a_old)
-               s2 = do t :*: sumT <- trainingCounts
-                       [psi (t `indexU` i + a_old), -psi (sumT + sumAs)]
-               w_new = w_old + step * a_old * kahanSum (s1 : s2)
-           writeMU ws     i w_new
-           writeMU new_as i (exp w_new) -- do not write in 'as'!
+               s2 = V.map (\(t, sumT) -> psi (t U.! i + a_old) -
+                                         psi (sumT + sumAs))
+                          trainingCounts
+               w_new = w_old + step * a_old * (s1 + V.sum s2)
+           MU.write ws     i w_new
+           MU.write new_as i (exp w_new) -- do not write in 'as'!
 
 -- | Cost function for deriving a Dirichlet density.  This
 --   function is minimized by 'deriveDD'.
-costDD :: DirichletDensity -> [TrainingVector] -> Double
-costDD (DD arr) tv = runST (go $ map (\t -> t :*: sumU t) tv)
-    where
-      size = lengthU arr
-      go tc = do alphas <- newMU size
-                 copyMU alphas 0 arr
-                 sumAs <- kahanSumMU alphas
-                 costDD' alphas sumAs tc
+costDD :: DirichletDensity -> V.Vector TrainingVector -> Double
+costDD (DD arr) tv = costDD' (U.length arr) arr (U.sum arr) $
+                     V.map (\t -> (t, U.sum t)) tv
 
 -- | 'costDD' needs to calculate the sum of all training vectors.
 --   This functios avoids recalculting this quantity in
 --   'deriveDD' multiple times.  This is the used by both
 --   'costDD' and 'deriveDD'.
-costDD' :: MUArr Double s -> Double -> [TrainingVector :*: Double] -> ST s Double
-costDD' alphas sumAs trainingCounts =
+costDD' :: Int -> U.Vector Double -> Double -> V.Vector (TrainingVector, Double) -> Double
+costDD' size alphas sumAs trainingCounts =
     let lngammaSumAs = lngamma sumAs
-        size = lengthMU alphas
-        f (t :*: sumT) = go 0 $ lngamma (sumT+1) + lngammaSumAs - lngamma (sumT + sumAs)
-            where go !i !acc | i == size = return acc
-                             | otherwise = do
-                    a_i <- readMU alphas i
-                    let t_i = t `indexU` i
-                    go (i+1) $ acc + lngamma (t_i + a_i) - lngamma (t_i + 1) - lngamma a_i
-    in (negate . kahanSum) `fmap` mapM f trainingCounts
+        f (t, sumT) = s + (U.sum $ U.zipWith g t alphas)
+            where s = lngamma (sumT+1) + lngammaSumAs - lngamma (sumT + sumAs)
+                  g t_i a_i = lngamma (t_i + a_i) - lngamma (t_i + 1) - lngamma a_i
+    in negate . V.sum $ V.map f trainingCounts
+{-# INLINE costDD' #-}
