9a69d3cf1e3e5e4cf8b6ed67b44945edcfb69278
felipe.lessa <felipe.lessa@gmail.com>
2010-03-26 00:39:37 +0000

    Only export functions from Mixture, never use Density.

--- Math/Statistics/Dirichlet.hs
+++ Math/Statistics/Dirichlet.hs
@@ -16,10 +16,10 @@
 module Math.Statistics.Dirichlet
     (-- * Data types (re-exported)
      DirichletMixture(..)
-    ,M.empty
-    ,M.Component
-    ,M.fromList
-    ,M.toList
+    ,empty
+    ,Component
+    ,fromList
+    ,toList
      -- * Options (re-exported)
     ,TrainingVector
     ,TrainingVectors
@@ -28,46 +28,10 @@ module Math.Statistics.Dirichlet
     ,Predicate(..)
     ,Reason(..)
     ,Result(..)
-    -- * Functions
+    -- * Functions (re-exported)
     ,derive
-    ,cost
-    -- * Util
-    ,DirichletDensity(..)
-    ,apply)
+    ,cost)
     where
 
-import qualified Math.Statistics.Dirichlet.Density as D
-import qualified Math.Statistics.Dirichlet.Mixture as M
-import Math.Statistics.Dirichlet.Density (DirichletDensity(..))
-import Math.Statistics.Dirichlet.Mixture (DirichletMixture(..))
+import Math.Statistics.Dirichlet.Mixture
 import Math.Statistics.Dirichlet.Options
-
-
-
--- | Applies a function to a Dirichlet mixture.  If the mixture
--- has only one component, then a special function for densities
--- is applied.
-apply :: (DirichletDensity -> a) -> (DirichletMixture -> a)
-      -> DirichletMixture -> a
-apply fd fm dm
-      | M.dmComponents dm == 1 = fd $ head $ M.dmDensitiesL dm
-      | otherwise              = fm dm
-
-
--- | Cost function for deriving a Dirichlet mixture (equation
--- 18).  This function is minimized by 'derive'.
-cost :: TrainingVectors -> DirichletMixture -> Double
-cost ns = apply (D.cost ns) (M.cost ns)
-
--- | Derive a Dirichlet mixture using a maximum likelihood method
--- as described by Karplus et al (equation 25).  All training
--- vectors should have the same length, however this is not
--- verified.
-derive :: DirichletMixture -- ^ Initial mixture.
-       -> Predicate        -- ^ Convergence test.
-       -> StepSize -> TrainingVectors -> Result DirichletMixture
-derive dm p s t = apply (\x -> change $ D.derive x p s t)
-                        (\x ->          M.derive x p s t) dm
-    where
-      change :: Result DirichletDensity -> Result DirichletMixture
-      change r = r {result = M.fromDD $ result r}
